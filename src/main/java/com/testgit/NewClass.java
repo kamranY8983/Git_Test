c5f3c4b595cddd8ace70b09959fb76bcfe66eafc
kamran Khan <kamranyusuf.khan04@gmail.com>
2021-08-14 07:52:52 +0530

    Commit to Branch Test1

---
+++ src/main/java/com/testgit/NewClass.java
@@ -0,0 +1,8 @@
+package com.testgit;
+
+public class NewClass {
+
+    public static void main(String[] args) {
+        System.out.println("Testing");
+    }
+}
--- src/main/java/com/testgit/Test1.java
+++ src/main/java/com/testgit/Test1.java
@@ -4,5 +4,6 @@ public class Test1 {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Added this Line to test new branch");
     }
 }
